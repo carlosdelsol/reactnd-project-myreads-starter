60c86ad09707ac3c7976d07e05187c41cb9079d2
Carlos del Sol <carlosdelsol@MacBook-Pro-de-Carlos.local>
2017-09-01 00:59:30 +0200

    getAll books from bookAPI

--- src/BookShelve.js
+++ src/BookShelve.js
@@ -3,8 +3,9 @@ import Book from './Book'
 
 class BookShelve extends Component {
   render() {
-    const { title, books } = this.props
-    
+    const { shelf, bookShelves } = this.props
+    const books = bookShelves.filter((b) => b.shelf === shelf);
+    const title = shelf === 'wantToRead' ? 'Want to Read' : (shelf === 'currentlyReading') ? 'Currently Reading' : 'Read';
     return (
         <div className="bookshelf">
           <h2 className="bookshelf-title">{ title }</h2>
@@ -12,7 +13,7 @@ class BookShelve extends Component {
             <ol className="books-grid">
                 {books.map((book, index) => (
                     <li key={index}>
-                        <Book cover={book.cover}
+                        <Book cover={book.imageLinks.thumbnail}
                               title={book.title}
                               authors={book.authors} />
                     </li>
